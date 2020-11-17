ce452868e9291435d4d7cee4073b0deb1ed41678
diegojuan2104 <diegojuan2104@gmail.com>
2020-11-17 00:56:56 -0500

    Adoption image galery ended, next: implement the specific characteristics

---
+++ src/components/mascota_card/mascota_card.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './mascota_card.styles.scss'
+
+const mascota_card = ({mascota}) => {
+
+  
+    return ( 
+    <div className="card_mascota" >
+                <div className="foto_mascota">
+                    <img 
+                        src={mascota.url_img}
+                    ></img>
+                </div>
+                <h4 className="card_title">{mascota.name}</h4>
+                
+    </div>);
+}
+ 
+export default mascota_card;
